49dffb5ce99aff9fe0b973c3945b98e70433c7be
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2019-12-03 09:19:15 +0900

    Dev 不要な出力を削除

--- layout.js
+++ layout.js
@@ -5,12 +5,8 @@ const layout = function layout(inp) {
     const button = document.getElementsByClassName("card_button")[0];
     let buttonHeight = parseFloat(getComputedStyle(button).height);
     for (let i = 0; i < cards.length; i++) {
-        console.log(parseFloat(getComputedStyle(content[i]).height)+"+"+buttonHeight+"=")
         cards[i].style.height = `${parseFloat(getComputedStyle(content[i]).height) + buttonHeight}px`
-        console.log(cards[i].style.height);
-        // card.style.height = "0px";
-    };
-    console.log(inp);
+    }
 }
 document.addEventListener("DOMContentLoaded", () => { layout("load") });
 window.addEventListener("resize", () => { layout("resize") });
