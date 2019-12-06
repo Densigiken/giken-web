85819ece8f303a090de5cbfa28f55e843611a526
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2019-12-06 18:17:31 +0900

    Design カードのデザインと挙動を調整

--- layout.js
+++ layout.js
@@ -1,12 +1,5 @@
 function layout() {
     document.documentElement.style.setProperty("--window-height", `${window.innerHeight}px`);
-    const cards = Array.prototype.slice.call(document.getElementsByClassName("card"));
-    const content = Array.prototype.slice.call(document.getElementsByClassName("card_layout"));
-    const button = document.getElementsByClassName("card_button")[0];
-    let buttonHeight = parseFloat(getComputedStyle(button).height);
-    for (let i = 0; i < cards.length; i++) {
-        cards[i].style.height = `${parseFloat(getComputedStyle(content[i]).height) + buttonHeight}px`
-    }
 }
 document.addEventListener("DOMContentLoaded", () => { layout() });
 window.addEventListener("resize", () => { layout() });
