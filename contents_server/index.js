a448a79a374970b77dba3cac69398822aacbea35
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2019-12-10 16:21:27 +0900

    Dev GETに用いるライブラリをhttpsからrequestに変更

--- contents_server/index.js
+++ contents_server/index.js
@@ -1,11 +1,11 @@
 const app = require('express')();
 const fs = require('fs');
-const https = require('https')
+const request = require('request')
 
 const accessToken = process.env.accessToken;
 const channelID = 'CR790EC3F';
-const blogurl = `https://slack.com/api/conversations.history?token=${accessToken}&channel=${channelID}&pretty=1`
-const userurl = `https://slack.com/api/users.list?token=${accessToken}&pretty=1`
+const blog_url = `https://slack.com/api/conversations.history?token=${accessToken}&channel=${channelID}&pretty=1`
+const user_url = `https://slack.com/api/users.list?token=${accessToken}&pretty=1`
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -18,68 +18,54 @@ app.get('/', (req, res) => {
     .send('Hi! Are you lost? Visit <a href="https://densigiken.github.io/giken-web/index.html">our website</a>')
 });
 
+
+const getjson = url => {
+  return new Promise((resolve, reject) => {
+    request({ url: url, method: 'GET', json: true }, (error, res, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+}
+
 app.get('/contents-refresh', (req, res) => {
   let contents = [];
-  let users = [];
-
-  https.get(userurl, (res) => {
-    let body = '';
-    res.setEncoding('utf8');
-
-    res.on('data', (chunk) => {
-      body += chunk;
-    });
-
-    res.on('end', (res) => {
-      users = JSON.parse(body).members;
-
-      https.get(blogurl, (res) => {
-        let body = '';
-        res.setEncoding('utf8');
-
-        res.on('data', (chunk) => {
-          body += chunk;
-        });
-
-        res.on('end', (res) => {
-          res = JSON.parse(body);
-          for (let i = 0; i < res.messages.length; i++) {
-            const message = res.messages[i];
-            if (message.type == 'message' && !message.subtype) {
-              const article = message.text.toString().split('\n');
-              contents[i] = { title: '', body: [] };
-              for (let j = 0; j < article.length; j++) {
-                if (j == 0) {
-                  contents[i].title = article[j];
-                } else {
-                  contents[i].body[j-1] = article[j];
-                }
-              }
+  getjson(user_url).then(users => {
+    getjson(blog_url).then(response => {
+      for (let i = 0; i < response.messages.length; i++) {
+        const message = response.messages[i];
+        if (message.type == 'message' && !message.subtype) {
+          const article = message.text.toString().split('\n');
+          contents[i] = { title: '', body: [] };
+          for (let j = 0; j < article.length; j++) {
+            if (j == 0) {
+              contents[i].title = article[j];
+            } else {
+              contents[i].body[j - 1] = article[j];
             }
           }
-          contents = contents.filter(v => v);
-          fs.writeFile('/tmp/blog.json', JSON.stringify(contents), (err, result) => {
-            if (err) {
-              console.log('error', err);
-            } else {
-              console.log('Blog data is ready!');
-              console.log(contents);
-            };
-          });
-        });
-      }).on('error', (e) => {
-        console.log(e.message);
+        }
+      }
+      contents = contents.filter(v => v);
+      fs.writeFile('/tmp/blog.json', JSON.stringify(contents), (err, result) => {
+        if (err) {
+          console.log('error', err);
+        } else {
+          console.log('Blog data is ready!');
+          console.log(contents);
+        }
       });
-      res
-        .status(202)
-        .send('Refresh process has been started.');
     });
-
-
-    });
+  });
+  res
+    .status(202)
+    .send('Refresh process has been started.');
   }).on('error', (e) => {
     console.log(e.message);
-  });
+});
 
 app.get('/blog', (req, res) => res.status(200).send(fs.readFileSync('/tmp/blog.json', 'utf-8')));
 
