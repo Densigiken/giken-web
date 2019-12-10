669b2ffc0544b96b7563a648e6d3b0abea25d598
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2019-12-10 15:20:28 +0900

    Dev ユーザー一覧を取得

--- contents_server/index.js
+++ contents_server/index.js
@@ -4,7 +4,8 @@ const https = require('https')
 
 const accessToken = process.env.accessToken;
 const channelID = 'CR790EC3F';
-const url = `https://slack.com/api/conversations.history?token=${accessToken}&channel=${channelID}&pretty=1`
+const blogurl = `https://slack.com/api/conversations.history?token=${accessToken}&channel=${channelID}&pretty=1`
+const userurl = `https://slack.com/api/users.list?token=${accessToken}&pretty=1`
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -18,8 +19,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/contents-refresh', (req, res) => {
-  https.get(url, (res) => {
-    let contents = [];
+  let contents = [];
+  let users = [];
+
+  https.get(userurl, (res) => {
     let body = '';
     res.setEncoding('utf8');
 
@@ -28,38 +31,55 @@ app.get('/contents-refresh', (req, res) => {
     });
 
     res.on('end', (res) => {
-      res = JSON.parse(body);
-      for (let i = 0; i < res.messages.length; i++) {
-        const message = res.messages[i];
-        if (message.type == 'message' && !message.subtype) {
-          const article = message.text.toString().split('\n');
-          contents[i] = { title: '', body: [] };
-          for (let j = 0; j < article.length; j++) {
-            if (j == 0) {
-              contents[i].title = article[j];
-            } else {
-              contents[i].body[j-1] = article[j];
+      users = JSON.parse(body).members;
+
+      https.get(blogurl, (res) => {
+        let body = '';
+        res.setEncoding('utf8');
+
+        res.on('data', (chunk) => {
+          body += chunk;
+        });
+
+        res.on('end', (res) => {
+          res = JSON.parse(body);
+          for (let i = 0; i < res.messages.length; i++) {
+            const message = res.messages[i];
+            if (message.type == 'message' && !message.subtype) {
+              const article = message.text.toString().split('\n');
+              contents[i] = { title: '', body: [] };
+              for (let j = 0; j < article.length; j++) {
+                if (j == 0) {
+                  contents[i].title = article[j];
+                } else {
+                  contents[i].body[j-1] = article[j];
+                }
+              }
             }
           }
-        }
-      }
-      contents = contents.filter(v => v);
-      fs.writeFile('/tmp/blog.json', JSON.stringify(contents), (err, result) => {
-        if (err) {
-          console.log('error', err);
-        } else {
-          console.log('Blog data is ready!');
-          console.log(contents);
-        };
+          contents = contents.filter(v => v);
+          fs.writeFile('/tmp/blog.json', JSON.stringify(contents), (err, result) => {
+            if (err) {
+              console.log('error', err);
+            } else {
+              console.log('Blog data is ready!');
+              console.log(contents);
+            };
+          });
+        });
+      }).on('error', (e) => {
+        console.log(e.message);
       });
+      res
+        .status(202)
+        .send('Refresh process has been started.');
+    });
+
+
     });
   }).on('error', (e) => {
     console.log(e.message);
   });
-  res
-    .status(202)
-    .send('Refresh process has been started.');
-});
 
 app.get('/blog', (req, res) => res.status(200).send(fs.readFileSync('/tmp/blog.json', 'utf-8')));
 
